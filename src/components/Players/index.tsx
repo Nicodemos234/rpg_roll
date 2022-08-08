45abe711276a52e8c2169fc997734edd661455bb
Vitor Rafael <rqfvitor@gmail.com>
2022-08-08 20:35:40 -0300

    feat: get players from graphql api

--- src/components/Players/index.tsx
+++ src/components/Players/index.tsx
@@ -5,15 +5,22 @@ import { Container } from '../Container';
 import { Modal } from '../Modal';
 import { Player } from './Player';
 
-export function Players() {
+interface PlayersProps {
+  players: {
+    name: string;
+    dexterity: number;
+  }[];
+}
+
+export function Players({ players }: PlayersProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
     <Container title="Jogadores" icon={<User size={28} />}>
       <section className="flex flex-col gap-2 mb-3">
-        <Player name="Aknorah" mod={12} />
-        <Player name="Demogorgon" mod={8} />
-        <Player name="Ami" mod={2} />
+        {players?.map(player => {
+          return <Player name={player.name} mod={player.dexterity} key={player.name} />;
+        })}
       </section>
       <button className="font-semibold transition-colors flex items-center justify-center gap-2 bg-yellow-500 text-zinc-800 p-2 rounded-md hover:bg-yellow-600" onClick={() => setIsModalVisible(true)}>
         <Plus size={22} weight="bold" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,50 @@
+import { gql } from '@apollo/client';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next/types';
 
 import { Header } from '../components/Header';
 import { Players } from '../components/Players';
 import { Rolls } from '../components/Rolls';
+import { client } from '../lib/apollo';
 
-export default function Home() {
+interface getPlayersQuery {
+  data: {
+    players: {
+      name: string;
+      dexterity: number;
+    }[];
+  };
+}
+
+export default function Home({ data }: getPlayersQuery) {
   return (
     <div className="flex flex-col items-center gap-16">
       <Head>
         <title>Rolling System</title>
       </Head>
-
       <Header title="Inicio" />
-
       <main className="flex w-[65%] justify-evenly gap-4">
-        <Players />
+        <Players players={data.players} />
         <Rolls />
       </main>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { data } = await client.query({
+    query: gql`
+      query GetPlayersQuery {
+        players {
+          name
+          dexterity
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      data,
+    },
+  };
+};
